59198b926a9eb9f6a53881cc6efed31d41a8f40d
rahuldeo03 <69627965+rahuldeo03@users.noreply.github.com>
2020-08-17 11:16:25 +0530

    Create Exercise9.py

---
+++ Exercise9.py
@@ -0,0 +1,17 @@
+''' Exercise 9
+Write a python function, encrypt_sentence(msg) which accepts a message and encrypts it based on rules given below and returns the encrypted message.
+ Words at odd position -> Reverse It
+Words at even position -> Rearrange the characters so that all consonants appear before the vowels and their order should not change
+Note: Assume that the sentence would begin with a word and there will be only a single space between the words.
+          Perform case sensitive string operations wherever necessary.
+Example: msg=the sun rises in the east    output=eht snu sesir ni eht stea
+
+Write a Python program to find the number of characters present the given string.
+
+Write a Python program to find the numbers of words present in the given sentence.
+
+Write a Python function words_count(sentence) to return a dictionary with the length of words as key and number words with such length as value.
+Example: sentence=“I love python and it so easy to learn” sample output={1:1,4:2,5:1,3:1,2:3,6:1}
+
+Write a Python function vowel_count(sentence) to return a dictionary with vowels, consonants,
+others as key and respective number of vowels, consonants, others characters as value. '''

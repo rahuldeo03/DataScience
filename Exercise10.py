052d2a6ac516c60ce85a73cee90ebe0c72083ae3
rahuldeo03 <69627965+rahuldeo03@users.noreply.github.com>
2020-08-17 11:21:57 +0530

    Create Exercise10.py

---
+++ Exercise10.py
@@ -0,0 +1,8 @@
+''' Exercise 10
+Write a program to read a file from a folder, 
+read from a url in write mode,
+print first 3 lines of the file by iterating
+save first 5 lines in a list,
+write 2 lines into the file,
+get current position of the file object pointer,
+close the file '''

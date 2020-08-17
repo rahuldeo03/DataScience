3fb57c878cd6eaf1ba96cfb50bc9e1046424ae21
rahuldeo03 <69627965+rahuldeo03@users.noreply.github.com>
2020-08-17 11:13:44 +0530

    Create Exercise8.py

---
+++ Exercise8.py
@@ -0,0 +1,16 @@
+''' Exercise8
+Write a Python function proper_divisors(num) which returns a list of all the proper divisors of given number.
+Example: Proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110
+
+Write a Python function generate_fibanacci(n) to return a list of first n Fibonacci numbers.
+
+Write a Python program to generate the next 15 leap years starting from a given year. Populate the leap years into a list and display the list.
+
+Given a string containing uppercase characters (A-Z), compress the string using Run Length encoding. Repetition of character has to be replaced by storing the length of that run.
+Write a python function encode(message)  which performs the run length encoding for a given String and returns the run length encoded String.
+Provide different String values and test your program.
+Example: message=AAAABBBBCCCCCCCC  output: 4A4B8C
+
+Represent a small bilingual (English-Swedish) glossary given below as a Python dictionary
+{"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"} 
+and use it to translate your Christmas wishes from English into Swedish. '''

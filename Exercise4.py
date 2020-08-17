0417962ece5aabbe8278dcdc83a654253dcd6cb5
rahuldeo03 <69627965+rahuldeo03@users.noreply.github.com>
2020-08-17 11:07:29 +0530

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,3 @@
+#Exercise -4
+#Write a python function, find_square() that accepts an integer number, n and returns the square of n. Invoke the function and display the square of the number. 
+#Write a python function, find_sum() that accepts an integer, n and returns the sum of first, n numbers. Invoke the function and display the sum of first n numbers.

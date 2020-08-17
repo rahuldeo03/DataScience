19c7c2e5ebfc31cb3e72ca0ad51d6706703c21c8
rahuldeo03 <69627965+rahuldeo03@users.noreply.github.com>
2020-08-17 11:06:37 +0530

    Create Exercise3.py

---
+++ Exercise3.py
@@ -0,0 +1,5 @@
+#Exercise3
+#Write a Python function to check whether the given year is leap year or not.
+#Write a Python function to find and display the maximum of three given numbers.
+#Write a Python function to check the given number is prime number or not.
+#Write a Python function to print first n Fibonacci numbers.
